340288d4e90f45d66512286aeca3644067167c3a
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-06-25 01:35:19 +0000

    [Finish] 5-0

--- readlines_practice.rb
+++ readlines_practice.rb
@@ -1,5 +1,11 @@
 lines = readlines
 lines.each do |words|
-  p words.chomp.split(' ')
+  p words.split(' ') # 改行文字も分割するために使用されるため、chompは不要
 end
 
+lines = readlines
+lines.each do |words|
+  words.chomp.split(' ').each do |word|
+    p word
+  end
+end
